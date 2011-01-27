9de5a81fbb3b6ef0696a3cafa55ec92222cd191a
Jake Hofman <jhofman+github@gmail.com>
2011-01-27 15:28:43 -0500

    slightly better error handling on boto script

--- code/ec2/create_instances.py
+++ code/ec2/create_instances.py
@@ -34,27 +34,43 @@ if __name__=='__main__':
     rsrv = images[0].run(1, num_instances, keypair)
 
     f = open('instances','w')
-    pending = rsrv.instances
-    while len(pending) > 0:
-        for instance in pending:
+    pending = list(rsrv.instances)
+    running = []
+    configured = []
+    while len(configured) < len(rsrv.instances):
+
+        print "pending:" , pending
+        print "running:" , running
+        print "configured: " , configured
+        for instance in rsrv.instances:
 
             print "checking" , instance.id
             instance.update()
 
             if instance.state == 'running':
-                print instance.id , "is up at" , instance.dns_name
-                pending.remove(instance)
-
-                f.write('%s %s\n' % (instance.id, instance.dns_name))
-
-                cmd="cat setup | ssh -o StrictHostKeyChecking=no -i ~/.ec2/*-%s ubuntu@%s &> %s.log &" % (keypair, instance.dns_name, instance.id)
-
-                print cmd
-                os.system(cmd)
-                
+                if instance not in running:
+                    print instance.id , "is up at" , instance.dns_name
+                    f.write('%s %s\n' % (instance.id, instance.dns_name))
+                    pending.remove(instance)
+                    running.append(instance)
             else:
                 print "still pending"
                 
+            if (instance in running) and (instance not in configured):
+                print "configuring" , instance.id
+                cmd="ssh -o StrictHostKeyChecking=no -i ~/.ec2/*-%s ubuntu@%s 'hostname'" % (keypair, instance.dns_name)
+                status = os.system(cmd)
+
+                if status == 0:
+                    cmd="cat setup | ssh -o StrictHostKeyChecking=no -i ~/.ec2/*-%s ubuntu@%s &> %s.log &" % (keypair, instance.dns_name, instance.id)
+                    print cmd
+                    status = os.system(cmd)
+
+                    configured.append(instance)
+                else:
+                    print "configuration failed"
+
+        print "sleeping"
         sleep(10)
 
     f.close()
