fd6b70d22f4ff0daa962cc01352901595520a609
Hilary Mason <h@bit.ly>
2011-01-29 19:09:01 -0500

    generate e-mail graph data, gmail module

---
+++ code/text_data/email_graph/email_edges.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+email_edges.py
+
+Created by Hilary Mason on 2011-01-29.
+Copyright (c) 2011 Hilary Mason. All rights reserved.
+"""
+
+import sys, os
+import csv
+from gmail import Gmail
+
+class email_edges(object):
+
+    def __init__(self, username, password):
+        g = Gmail(username, password)
+        
+        viewed_messages = []
+        for folder in g.list_folders(): # iterate through all folders in the account
+            # print "%s: %s" % (folder, g.get_message_ids(folder)) # NOTE: uncomment this to see which ids are in each folder
+            for message_id in g.get_message_ids(folder): # iterate through message IDs
+                if message_id not in viewed_messages: # ...but don't repeat messages
+                    # print "Processing %s" % message_id
+                    msg = g.get_message(message_id)
+                    
+                    for line in msg.split('\n'):
+                        line = line.strip()
+                        if line[0:5] == "From:":
+                            msg_from = line[5:].strip()
+                        elif line[0:3] == "To:":
+                            msg_to = line[3:].strip()
+                        
+                    try:
+                        print "%s, %s" % (msg_from, msg_to) # TODO: use csv?
+                    except UnboundLocalError:
+                        pass
+                        
+        
+
+if __name__ == '__main__':
+    username = 'ann9enigma@gmail.com'
+    password = 'stratar0x'
+	
+    e = email_edges(username, password)
+
