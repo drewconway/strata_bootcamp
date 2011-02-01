7e4656ec75069527b7a26127748473bbbad22d17
Drew Conway <agconway@gmail.com>
2011-02-01 12:04:41 -0500

    last minute

--- code/mashups/strata_map.R
+++ code/mashups/strata_map.R
@@ -20,7 +20,7 @@ library(maps)
 strata.clicks<-read.csv("../../data/strata_lat_lon", header=FALSE, col.names=c("lat","lon"))
 
 # Create a frequnecy count of each lat-lon combo
-click.counts<-ddply(strata.clicks,.(lat,lon), nrow)
+click.counts<-ddply(strata.clicks,.(lat,lon), "nrow")
 
 
 # Get map of the world
@@ -30,8 +30,8 @@ world.map<-data.frame(map(plot=FALSE)[c("x","y")])
 click.plot<-ggplot(world.map, aes(x=x,y=y))+geom_path(aes(colour="grey"))
 
 # Add click points to map
-click.plot<-click.plot+geom_point(data=click.counts, aes(x=lon, y=lat, color="red", alpha=0.65, size=V1))+
-    scale_colour_manual(values=c("grey"="grey","red"="red"),legend=FALSE)+scale_alpha(legend=FALSE)+scale_size(legend=FALSE)+
+click.plot<-click.plot+geom_point(data=click.counts, aes(x=lon, y=lat, color="red", alpha=0.65, size=nrow))+
+    scale_colour_manual(values=c("grey"="grey","red"="red"),legend=FALSE)+scale_alpha(legend=FALSE)+
     coord_map(projection="lagrange",ylim=c(-40,70),xlim=c(-145,155))+theme_bw()+
     opts(panel.grid.major=theme_blank(),axis.ticks=theme_blank(),axis.text.x=theme_blank(),axis.text.y=theme_blank())+
     xlab("")+ylab("")
